b692cb0b808b31c707b2745efdb9fbe43d320dd4
Brofistansh <jaiansh2016@gmail.com>
2023-06-20 02:46:42 +0530

    countdown added with sound effects

--- Assets/Scripts/scriptsgame/Player/PlayerMove.cs
+++ Assets/Scripts/scriptsgame/Player/PlayerMove.cs
@@ -6,23 +6,27 @@ public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 8;
     public float leftRightSpeed = 4;
-  
+    public static bool canMove = false;
+
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (canMove == true)
         {
-            if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
-            { 
-                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
-            }
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
-                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+                if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
+                {
+                    transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+                }
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+                {
+                    transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+                }
             }
         }
     }
