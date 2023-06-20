bbd921eec7a8afd9d02c8bc47bc8c9eb9e8ef81f
Brofistansh <jaiansh2016@gmail.com>
2023-06-20 12:36:58 +0530

    gameover screen added

--- Assets/Scripts/scriptsgame/Enviornment/Obst.cs
+++ Assets/Scripts/scriptsgame/Enviornment/Obst.cs
@@ -8,13 +8,16 @@ public class Obst: MonoBehaviour
     public GameObject charModel;
     public AudioSource crashThud;
     public GameObject mainCam;
+    public GameObject levelControl;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         thePlayer.GetComponent<PlayerMove>().enabled = false;
         charModel.GetComponent<Animator>().Play("Stumble Backwards");
+        levelControl.GetComponent<LevelDistance>().enabled = false;
         crashThud.Play();
         mainCam.GetComponent<Animator>().enabled = true;
+        levelControl.GetComponent<EndRunSequence>().enabled = true; 
     }
 }
--- Assets/Scripts/scriptsgame/collectibles/LevelDistance.cs
+++ Assets/Scripts/scriptsgame/collectibles/LevelDistance.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class LevelDistance : MonoBehaviour
 {
     public GameObject disDisplay;
+    public GameObject disEndDisplay;
     public int disRu;
     public bool addingDis = false;
     void Update()
@@ -20,6 +21,7 @@ public class LevelDistance : MonoBehaviour
     {
         disRu += 1;
         disDisplay.GetComponent<Text>().text = "" + disRu;
+        disEndDisplay.GetComponent<Text>().text = "" + disRu;
         yield return new WaitForSeconds(0.25f);
         addingDis = false;
     }
